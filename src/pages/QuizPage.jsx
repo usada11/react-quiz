a54bc117a3e7f6ba4bc747d0c59411cbbd8e0019
2132055 <s2132055ah@s.chibakoudai.jp>
2026-04-22 19:25:27 +0900

    遷移画面

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,29 +1,45 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../components/Button/Button";
 import Display from "../components/Display/Display";
 import quizData from "../data/quiz";
+import { useEffect } from "react";
+import { useState } from "react";
+import { ROUTES } from "../const";
 
 export default function QuizPage() {
   const [quizIndex, setQuizIndex] = useState(0);
   const [answerLogs, setAnswerLogs] = useState([]);
+  const navigation = useNavigate();
+  const MAX_QUIZ_LEN = quizData.length;
 
   const handleClick = (clickedIndex) => {
     if (clickedIndex === quizData[quizIndex].answerIndex) {
-      setAnswerLogs((prev) => [...prev, true])
+      setAnswerLogs(prev => [...prev, true]);
     } else {
-      setAnswerLogs((prev) => [...prev,false])
+      setAnswerLogs(prev => [...prev, false]);
     }
-    setQuizIndex((prev)=>prev+1)
+    setQuizIndex(prev => prev + 1);
   };
+
+  useEffect(() => {
+    if (answerLogs.length === MAX_QUIZ_LEN) {
+      const correctNum = answerLogs.filter(answer => answer == true);
+      navigation(ROUTES.RESULT, {
+        state: {
+          maxQuizlen: MAX_QUIZ_LEN,
+          correctNum: correctNum
+        },
+      });
+    }
+  }, [answerLogs, MAX_QUIZ_LEN, navigation]);
+
   return (
     <>
-      <Display>{`Q1.${quizData[quizIndex].question}`}</Display>
-      {quizData[quizIndex].options.map((option, index) => {
-        return (
-          <Button key={`option-${index}`} onClick={() => handleClick(index)}>
-            {option}
-          </Button>
-        );
-      })}
+      {quizData[quizIndex] && <Display>{`Q.${quizIndex +1} ${quizData[quizIndex].question}`}</Display>}
+      <br/>
+      {quizData[quizIndex] && quizData[quizIndex].options.map((option, index) => 
+          <Button key={`option-${index}`} onClick={() => handleClick(index)}>{option}</Button>
+      )}
     </>
   );
 }
