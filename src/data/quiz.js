85c3a20d246640f428387b483d5d287c5a113f14
2132055 <s2132055ah@s.chibakoudai.jp>
2026-04-23 12:29:06 +0900

    cssアニメーション追加等

--- src/data/quiz.js
+++ src/data/quiz.js
@@ -1,12 +1,12 @@
 const quizData = [
   {
-    question: "Question 1",
-    options: ["Select 1", "Serect 2", "Select 3", "Serect 4"],
+    question: "set関数でstate変数を更新を更新する際に何を使うと予期せぬバグが防げる？",
+    options: ["関数型更新", "スプレッド構文", "useEffect", "useState"],
     answerIndex: 0
   },
   {
-    question: "Question 2",
-    options: ["Select 1", "Serect 2", "Select 3", "Serect 4"],
+    question: "何を使うとより簡単な書き方で配列に要素を追加できる？",
+    options: ["関数型更新", "スプレッド構文", "useEffect", "useState"],
     answerIndex: 1
   },
   {
--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,17 +1,24 @@
 import { Link, useLocation } from "react-router-dom";
 import { ROUTES } from "../const";
 import Result from "../components/Result/Result";
+import Loading from "../Loading/Loading";
+import { useEffect, useState } from "react";
+
 
 export default function ResultPage() {
+  const [active, setActive] = useState(false);
   const location = useLocation();
   const maxQuizLen = location.state.maxQuizLen;
   const correctNumLen = location.state.correctNumLen;
 
+  useEffect(() => {
+    setTimeout(() => setActive(true),3000)
+  },[])
+
   return (
     <>
+      <Loading active={active} />
       <h1>Result</h1>
-      <p>あなたの正解数は...</p>
-      <p>{`全${maxQuizLen}問中，${correctNumLen}問正解でした！`}</p>
       <Result maxQuizLen={maxQuizLen} correctNumLen={ correctNumLen} />
       <br />
       <Link to={ROUTES.QUIZ}>もう一度チャレンジ</Link>
